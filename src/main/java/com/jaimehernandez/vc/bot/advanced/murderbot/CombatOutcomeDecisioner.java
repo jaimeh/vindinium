a189a0f3fd3247664ea8f9175dcda4aaed95a54d
Jaime Hernandez <jaime@jaimehernandez.com>
2015-12-12 09:16:44 -0800

    Adjust health decision when attacking

--- src/main/java/com/jaimehernandez/vc/bot/advanced/murderbot/CombatOutcomeDecisioner.java
+++ src/main/java/com/jaimehernandez/vc/bot/advanced/murderbot/CombatOutcomeDecisioner.java
@@ -40,7 +40,7 @@ public class CombatOutcomeDecisioner implements Decision<AdvancedMurderBot.GameC
         }
 
         // If we have more health than them or they only can take one more hit
-        if(opponent.getLife() <= 20 || (me.getLife() + 30) > opponent.getLife()) {
+        if(opponent.getLife() <= 20 || (me.getLife() + 50) > opponent.getLife()) {
             logger.info("Bot will most likely win the current battle.");
             return winningDecisioner.makeDecision(context);
         }  else {
